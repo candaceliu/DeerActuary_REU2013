e60ef30ec886090b990cb0e34414f9a90b65e71b
Kelly Black <kjblack@gmail.com>
2013-10-18 10:12:20 -0400

    Set up an R file that will generate all of the graphs from the data set.

--- Paper/Model/presentation/data/results.R
+++ Paper/Model/presentation/data/results.R
@@ -11,7 +11,7 @@ results$sx = ((results$sumx2-(results$sumx^2)/results$N)/(results$N-1))
 results$sx[results$sx<0] = 0.0
 results$sx = sqrt(results$sx)
 
-alpha = unique(results$alpha)
+alphaVals = unique(results$alpha)
 P     = unique(results$P)
 
 maxM = max(results$mx)
@@ -19,37 +19,102 @@ minM = min(results$mx)
 maxS = max(results$sx)
 minS = min(results$sx)
 
-plot(results$alpha[results$P == 430000],results$mx[results$P == 430000])
-
-
-# Display the mean deer populations for different values of P
-plot(0,0,ylim=c(minM,maxM),xlim=c(min(results$alpha),max(results$alpha)),col=0,pch=0,
-     xlab=expression(alpha),ylab='Sample Mean of the Deer Population',
-     main=expression(paste("Sample Mean of the Deer Population vs. ",alpha)))
-symbol = 0
-for (plevel in P)
+filename = "fullParameters.png"
+if(!file.exists(filename))
   {
-    symbol = (symbol + 1)%%30
-    symbol = 1
-    points(results$alpha[results$P == plevel],results$mx[results$P == plevel],
-           col=symbol,pch=symbol)
-  }
-#stop("yo!")
-
-# Display the std. dev of the deer populations for different values of P
-plot(0,0,ylim=c(minS,maxS),xlim=c(min(results$alpha),max(results$alpha)),col=0,pch=0,
-     xlab=expression(alpha),ylab='Sample Standard Deviation of the Deer Population',
-     main=expression(paste("Sample Standard Deviation of the Deer Population vs. ",alpha)))
-symbol = 0
-for (plevel in P)
-  {
-    symbol = (symbol + 1)%%30
-    symbol = 1
-    points(results$alpha[results$P == plevel],results$sx[results$P == plevel],
-           col=symbol,pch=symbol)
+    png(filename,width=1024,height=1024)
+    plot(0,0,pch=0,xlim=c(430000,529333),ylim=c(0.0,0.15),
+         main="Parameter Space",xlab="P",ylab=expression(alpha))
+    for(P in seq(430000,530000,by=(530000-430000)/150))
+      {
+        for (a in seq(0.0,0.15,by=0.15/50))
+          {
+            points(P,a,pch=18,cex=0.6)
+          }
+      }
+    dev.off()
   }
 
 
+filename = "meanDeerByAlpha.png"
+if(!file.exists(filename))
+  {
+    # Display the mean deer populations for different values of P
+    png(filename,width=1024,height=1024)
+    plot(0,0,ylim=c(minM,maxM),xlim=c(min(results$alpha),max(results$alpha)),col=0,pch=0,
+         xlab=expression(alpha),ylab='Sample Mean of the Deer Population',
+         main=expression(paste("Sample Mean of the Deer Population vs. ",alpha)))
+    symbol = 0;
+    legendSymbols = double(0)
+    legendText = character(0)
+    for (P in seq(430000,530000,by=15*(530000-430000)/150))
+      {
+        symbol = symbol+1
+        legendSymbols = c(legendSymbols,symbol)
+        legendText = c(legendText,paste("P=",P))
+        points(results$alpha[results$P == P],results$mx[results$P == P],
+               pch=symbol,col=symbol)
+      }
+    legend(0,20100,legendText,pch=legendSymbols,col=legendSymbols)
+                                        #stop("a")
+    dev.off()
+  }
+
+
+filename = "meanDeerByAlpha_boxplot.png"
+if(!file.exists(filename))
+  {
+    png(filename,width=1024,height=1024)
+    plot(0,0,ylim=c(minM,maxM),xlim=c(min(results$alpha),max(results$alpha)),col=0,pch=0,
+         xlab=expression(alpha),ylab='Sample Mean of the Deer Population',
+         main=expression(paste("Sample Mean of the Deer Population vs. ",alpha)))
+    for (a in alphaVals[seq(1,length(alphaVals),by=3)])
+      {
+        boxplot(results$mx[results$alpha == a],add=TRUE,at=a,boxwex=0.015)
+      }
+                                        #stop("a")
+    dev.off()
+  }
+
+
+filename = "stdDevDeerbyAlpha.png"
+if(!file.exists(filename))
+  {
+    # Display the std. dev of the deer populations for different values of P
+    png(filename,width=1024,height=1024)
+    plot(0,0,ylim=c(minS,maxS),xlim=c(min(results$alpha),max(results$alpha)),col=0,pch=0,
+         xlab=expression(alpha),ylab='Sample Standard Deviation of the Deer Population',
+         main=expression(paste("Sample Standard Deviation of the Deer Population vs. ",alpha)))
+    symbol = 0;
+    legendSymbols = double(0)
+    legendText = character(0)
+    for (P in seq(430000,530000,by=(530000-430000)/150))
+      {
+        symbol = (symbol + 1)%%30
+        legendSymbols = c(legendSymbols,symbol)
+        legendText = c(legendText,paste("P=",P))
+        points(results$alpha[results$P == P],results$sx[results$P == P],
+               col=symbol,pch=symbol)
+      }
+                                        #stop("a")
+    dev.off()
+  }
+
+
+filename = "stdDevDeerbyAlpha_boxplot.png"
+if(!file.exists(filename))
+  {
+    png(filename,width=1024,height=1024)
+    plot(0,0,ylim=c(minS,maxS),xlim=c(min(results$alpha),max(results$alpha)),col=0,pch=0,
+         xlab=expression(alpha),ylab='Sample Standard Deviation of the Deer Population',
+         main=expression(paste("Sample Standard Deviation of the Deer Population vs. ",alpha)))
+    for (a in alphaVals[seq(1,length(alphaVals),by=3)])
+      {
+        boxplot(results$sx[results$alpha == a],add=TRUE,at=a,boxwex=0.015)
+      }
+                                        #stop("a")
+    dev.off()
+  }
 
 results$mF = results$summ/results$N*100.0;
 results$sFx = ((results$summ2-(results$summ^2)/results$N)/(results$N-1))*10000.0;
@@ -63,31 +128,134 @@ maxsF = max(results$sFx)
 minsF = min(results$sFx)
 
 levelplot(results$mF~results$alpha*results$P,contour=TRUE,
-          xlab=expression(alpha),ylab='P')
+          xlab=expression(alpha),ylab='P',main="Sample Mean Fund Balance")
 levelplot(results$sFx~results$alpha*results$P,contour=TRUE,
-          xlab=expression(alpha),ylab='P')
+          xlab=expression(alpha),ylab='P',main="Sample Std. Dev. Fund Balance")
 
-# Display the mean deer populations for different values of P
-plot(0,0,ylim=c(minF,maxF),xlim=c(min(results$P),max(results$P)),col=0,pch=0,
-     xlab="P",ylab='Sample Mean of the Deer Population',
-     main=expression(paste("Sample Mean of the Deer Population vs. ","P")))
-symbol = 0
-for (a in alpha)
+filename = "meanfundBalanceByP.png"
+if(!file.exists(filename))
   {
-    symbol = (symbol + 1)%%30
-    points(results$P[results$alpha == a],results$mF[results$alpha == a],
-           col=symbol,pch=symbol)
+    # Display the mean deer populations for different values of P
+    png(filename,width=1024,height=1024)
+    plot(0,0,ylim=c(minF,maxF),xlim=c(min(results$P),max(results$P)),col=0,pch=0,
+         xlab="P",ylab='Sample Mean of the Fund Level',
+         main=expression(paste("Sample Mean of the Deer Population vs. ","P")))
+    symbol = 0
+    legendSymbols = double(0)
+    legendText = character(0)
+    for (a in alphaVals[seq(1,length(alphaVals),by=3)])
+      {
+        symbol = (symbol + 1)%%30
+        legendSymbols = c(legendSymbols,symbol)
+        legendText = c(legendText,as.expression(bquote(alpha == .(a))))
+        points(results$P[results$alpha == a],results$mF[results$alpha == a],
+               col=symbol,pch=symbol)
+      }
+    legend(430000,5.6E7,legendText,pch=legendSymbols,col=legendSymbols)
+                                        #stop("a")
+    dev.off()
   }
 
 
-# Display the standard dev. of the deer populations for different values of alpha
-plot(0,0,ylim=c(minsF,maxsF),xlim=c(min(results$P),max(results$P)),col=0,pch=0,
-     xlab="P",ylab='Sample Mean of the Deer Population',
-     main=expression(paste("Standard Deviation of the Deer Population vs. ","P")))
-symbol = 0
-for (a in alpha)
+filename = "meanFundBalanceByP_boxplot.png"
+if(!file.exists(filename))
   {
-    symbol = (symbol + 1)%%30
-    points(results$P[results$alpha == a],results$sFx[results$alpha == a],
-           col=symbol,pch=symbol)
+    png(filename,width=1024,height=1024)
+    plot(0,0,ylim=c(minF,maxF),xlim=c(min(results$P),max(results$P)),col=0,pch=0,
+         xlab="P",ylab='Sample Mean of the Fund Level',
+         main=expression(paste("Sample Mean of the Deer Population vs. ","P")))
+    for (P in seq(430000,530000,by=10*(530000-430000)/150))
+      {
+        boxplot(results$mF[abs(results$P-P)<1.0],add=TRUE,at=P,boxwex=10000)
+      }
+                                        #stop("a")
+    dev.off()
+  }
+
+
+filename = "meanFundBalanceByAlpha.png"
+if(!file.exists(filename))
+  {
+    # Display the mean deer populations for different values of alpha
+    png(filename,width=1024,height=1024)
+    #cat("yo\n")
+    plot(0,0,ylim=c(minF,maxF),xlim=c(min(results$alpha),max(results$alpha)+0.05),col=0,pch=0,
+         xlab=expression(alpha),ylab='Sample Mean of the Fund Level',
+         main=expression(paste("Sample Mean of the Deer Population vs. ",alpha)))
+    symbol = 0
+    legendSymbols = double(0)
+    legendText = character(0)
+    for (a in alphaVals[seq(1,length(alphaVals),by=3)])
+      {
+        symbol = (symbol + 1)%%30
+        legendSymbols = c(legendSymbols,symbol)
+        legendText = c(legendText,as.expression(bquote(alpha == .(a))))
+        points(results$alpha[results$alpha == a],results$mF[results$alpha == a],
+               col=symbol,pch=symbol)
+      }
+    par(xpd=TRUE)
+    legend(0.16,5.6E7,legendText,pch=legendSymbols,col=legendSymbols)
+    par(xpd=FALSE)
+                                        #stop("a")
+    dev.off()
+  }
+
+
+filename = "meanfundBalanceByAlpha_boxplot.png"
+if(!file.exists(filename))
+  {
+    png(filename,width=1024,height=1024)
+    plot(0,0,ylim=c(minF,maxF),xlim=c(min(results$alpha),max(results$alpha)),col=0,pch=0,
+         xlab=expression(alpha),ylab='Sample Mean of the Fund Level',
+         main=expression(paste("Sample Mean of the Deer Population vs. ",alpha)))
+    for (a in alphaVals[seq(1,length(alphaVals),by=3)])
+      {
+        boxplot(results$mF[results$alpha == a],add=TRUE,at=a,boxwex=0.01)
+      }
+                                        #stop("a")
+    dev.off()
+  }
+
+
+filename = "sdFundBalanceByP.png"
+if(!file.exists(filename))
+  {
+    # Display the standard dev. of the deer populations for different values of alpha
+    png(filename,width=1024,height=1024)
+    plot(0,0,ylim=c(minsF,maxsF),xlim=c(min(results$P),max(results$P)+11000),col=0,pch=0,
+         xlab="P",ylab='Sample Standard Deviaion of the Fund Value',
+         main=expression(paste("Standard Deviation of the Fund Value vs. ","P")),
+         bty='L')
+    symbol = 0
+    legendSymbols = double(0)
+    legendText = character(0)
+    for (a in alphaVals[seq(1,length(alphaVals),by=3)])
+      {
+        symbol = (symbol + 1)%%30
+        legendSymbols = c(legendSymbols,symbol)
+        legendText = c(legendText,as.expression(bquote(alpha == .(a))))
+        points(results$P[results$alpha == a],results$sFx[results$alpha == a],
+               col=symbol,pch=symbol)
+      }
+    par(xpd=TRUE)
+    legend(531000,50,legendText,pch=legendSymbols,col=legendSymbols)
+    par(xpd=FALSE)
+    dev.off()
+  }
+
+
+
+filename = "sdfundBalanceByP_boxplot.png"
+if(!file.exists(filename))
+  {
+    png(filename,width=1024,height=1024)
+    plot(0,0,ylim=c(minsF,maxsF),xlim=c(min(results$P),max(results$P)+11000),col=0,pch=0,
+         xlab="P",ylab='Sample Standard Deviaion of the Fund Value',
+         main=expression(paste("Standard Deviation of the Fund Value vs. ","P")),
+         bty='L')
+    for (P in seq(430000,530000,by=10*(530000-430000)/150))
+      {
+        boxplot(results$sFx[abs(results$P-P)<1.0],add=TRUE,at=P,boxwex=10000)
+      }
+    dev.off()
   }
